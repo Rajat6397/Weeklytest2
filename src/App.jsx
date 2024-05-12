fbfdb70b2c8ca5491b39fe5b6536054f4ec3fb6d
Rajat <rajatsaxena6397@gmail.com>
2024-05-12 23:54:11 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+import Navbar from "./Component/Navbar/Navbar";
+function App() {
+
+<Navbar/>
+ 
+}
+
+export default App;
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+ import './Navbar.css'
+  
+const Navbar = () =>{
+    return (
+        <nav className='navbar'>
+         <div className='desktopmenu'>
+        
+          <h1>Hello</h1>
+         </div>
+         <button className='desktonmenumenubtn'></button>
+        </nav>
+    )
+}
+
+export default Navbar;
